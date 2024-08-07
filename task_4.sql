9ab3c64cb1c2cbcdacffd9183e0df0ee503ff73d
temitopefa <lasbohoo@gmail.com>
2024-08-07 21:28:27 +0100

    task 4

--- task_4.sql
+++ task_4.sql
@@ -3,8 +3,8 @@
 -- Select the alx_book_store database
 USE alx_book_store;
 
--- Query information_schema to get the full description of the books table
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+-- Query information_schema to get the full description of the Books table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
 WHERE TABLE_SCHEMA = 'alx_book_store'
-AND TABLE_NAME = 'books';
+AND TABLE_NAME = 'Books';
